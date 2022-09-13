916db4f5379294ed658692c76337115af5b3dbb7
Sitthichok Saelim <sitthichok.sael@kmutt.ac.th>
2022-09-13 18:27:58 +0700

    1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,11 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(startWord==Text&&word==null){
+    return undefined
+}else if(startWord==Text&&word==undefined){
+  return undefined
 }
+}
+
 module.exports = fillStartWord
